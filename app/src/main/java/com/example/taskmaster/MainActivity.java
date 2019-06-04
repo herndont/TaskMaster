d02cb37af04c0e124271741d703f10908f00ada4
herndont <herndontrey@gmail.com>
2019-06-04 13:32:47 -0700

    got stuck with some technical issues this afternoon and had Kell help me out... Need to complete the lab and fix the other cosmetic areas that are currently throwing errors

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.taskmaster;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        dl = (DrawerLayout)findViewById(R.id.activity_main);
+        t = new ActionBarDrawerToggle(this, dl,R.string.open, R.string.close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id)
+                {
+//                    case R.id.nav_item_1:
+//                        Intent intent = new Intent(MainActivity.this, FingerActivity.class);
+//                        startActivity(intent);
+//                        break;
+
+                    case R.id.nav_item_2:
+                        Toast.makeText(MainActivity.this, "You're already at Home!",Toast.LENGTH_SHORT).show();break;
+
+//                    case R.id.nav_item_3:
+//                        Intent intent2 = new Intent(MainActivity.this, ExerciseDiaryActivity.class);
+//                        startActivity(intent2);
+//                        break;
+//
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 }
